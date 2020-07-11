509e0a4624f28cd542b0b1859e394f26fc12b222
Aakash Karki <47515796+Derslived@users.noreply.github.com>
2020-07-11 16:04:11 +0545

    Double List (part II)
    This is a Node Object made for Double List.

--- Node.java
+++ Node.java
@@ -1,32 +1,39 @@
-package com.groot.day2.util;
 
+public class Node {
 
-public class Node{
-
+    public Node previous;
     public int data;
     public Node next;
 
-    public Node(int d){
-        data=d;
-        next= null;
+    public Node(int d) {
+        previous = null;
+        data = d;
+        next =null;
+
     }
 
-    public void setDate(int data){
-        this.data= data;
+    public void setData(int data) {
+        this.data = data;
     }
 
-    public int getData(){
+    public int getData() {
         return data;
     }
 
-    public void setNext(Node next){
-        this.next =next;
+    public void setNext(Node next) {
+        this.next = next;
     }
 
-    public Node getNext(){
+    public Node getNext() {
         return next;
     }
 
+    public void setprevious(Node previous) {
+        this.previous = previous;
+    }
 
+    public Node getprevious() {
+        return previous;
+    }
 
 }
